3ec28cf551844ae6e9e65a761b71fa178da072e0
Praveen <praveenrajashekar134@example.com>
2025-06-03 14:31:21 +0530

    git externals

--- main_app/apps.py
+++ main_app/apps.py
@@ -2,4 +2,6 @@ from django.apps import AppConfig
 
 
 class MainAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'main_app'
+    verbose_name = "Main Application"
